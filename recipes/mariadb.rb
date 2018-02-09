500305da73137430330d7b5ddb14499b8153f7d4
markliu152 <1099134352@qq.com>
2018-02-09 22:42:30 +0800

    configure default.rb

--- recipes/mariadb.rb
+++ recipes/mariadb.rb
@@ -1,8 +1,9 @@
 include_recipe 'zabbix1::mysql'
 
-execute 'zcat_create' do
- command <<-"EOH"
-   zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u#{node['zabbix']['mysql']['user_name']} -p#{node['zabbix']['mysql']['user_password']} #{node['zabbix']['mysql']['db_name']}
+bash 'zcat_create' do
+  user 'root'
+  command <<-"EOH"
+    zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u#{node['zabbix']['mysql']['user_name']} -p#{node['zabbix']['mysql']['user_password']} #{node['zabbix']['mysql']['db_name']}
   EOH
-  only_if "mysql -u root -p#{node['zabbix']['mysql']['root_password']} -D #{node['zabbix']['mysql']['db_name']}"
+  only_if "mysqlshow --user=root --password=#{node['zabbix']['mysql']['root_password']} #{node['zabbix']['mysql']['db_name']}"
 end
