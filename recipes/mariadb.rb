f1664a871f1dc36306bcd7e85d524435dce0a27e
markliu152 <1099134352@qq.com>
2018-02-09 21:51:36 +0800

    configure mariadb.rb

--- recipes/mariadb.rb
+++ recipes/mariadb.rb
@@ -1,6 +1,6 @@
 include_recipe 'zabbix1::mysql'
 
-bash 'zcat_create' do
+execute 'zcat_create' do
  command <<-"EOH"
    zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u#{node['zabbix']['mysql']['user_name']} -p#{node['zabbix']['mysql']['user_password']} #{node['zabbix']['mysql']['db_name']}
   EOH
